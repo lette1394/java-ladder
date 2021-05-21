122cb5836e946ba1875e4fd47572416de72c5500
lette1394 <lette1394@gmail.com>
2021-05-21 21:27:43 +0900

    chore: add Contracts class

---
+++ src/main/java/com/github/lette1394/Contracts.java
@@ -0,0 +1,18 @@
+package com.github.lette1394;
+
+import static java.lang.String.format;
+
+public final class Contracts {
+  public static void requires(boolean truthy, String message, Object... arguments) {
+    if (truthy) {
+      return;
+    }
+    throw new ContractsViolationException(format(message, arguments));
+  }
+}
+
+class ContractsViolationException extends RuntimeException {
+  public ContractsViolationException(String message) {
+    super(message);
+  }
+}
